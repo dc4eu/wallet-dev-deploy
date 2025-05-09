e4b89fe91a37e0baabe4a4ca3a4a147096b65ed0
Simon <contact@smn.codes>
2025-05-09 17:20:14 +0200

    Pull wallet-ecosystem repo

--- pull-latest.sh
+++ pull-latest.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+ROOT_DIR=$PWD
+
 declare -A repos
 
 repos=(
     ['wallet-frontend']='https://github.com/wwwallet/wallet-frontend'
     ['wallet-backend-server']='https://github.com/wwwallet/wallet-backend-server'
+    ['wallet-ecosystem']='https://github.com/wwwallet/wallet-ecosystem'
 )
 
 for key in ${!repos[@]}; do
-    if [ ! -d "$PWD/apps/$key" ]; then
-        git clone ${repos[${key}]} apps/$key
+    if [ ! -d "$ROOT_DIR/apps/$key" ]; then
+        git clone --recurse-submodules ${repos[${key}]} $ROOT_DIR/apps/$key
     else
-        cd $PWD/apps/$key && git pull;
+        cd $ROOT_DIR/apps/$key && git pull --recurse-submodules;
     fi
 done
