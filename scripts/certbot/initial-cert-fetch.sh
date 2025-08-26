3e16a5df1e51ac183abea463d8334a2877a12e69
Simon <contact@smn.codes>
2025-08-26 11:01:52 +0200

    Remove unused script

--- scripts/certbot/initial-cert-fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# =========================================================================
-# Insert demo Issuer and Verifier data into Wallet database
-# =========================================================================
-
-docker compose run --publish 80:80 certbot;
